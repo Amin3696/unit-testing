0784cb5ff46e9983497ada2a22680f69ee8eebb3
Amin Shahimian <43721334+Amin3696@users.noreply.github.com>
2021-07-13 13:41:24 +0200

    add Unit Test for Controller

--- src/main/java/com/unitTestimng/unittesting/controller/HelloWorldController.java
+++ src/main/java/com/unitTestimng/unittesting/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.unitTestimng.unittesting;
+package com.unitTestimng.unittesting.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
