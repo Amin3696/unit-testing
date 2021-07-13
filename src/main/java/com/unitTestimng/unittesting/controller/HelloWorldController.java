9dc2a1eb8673122b35b74bd6a1cc88164147676c
Amin Shahimian <43721334+Amin3696@users.noreply.github.com>
2021-07-13 15:48:33 +0200

    re add HelloWorldController

---
+++ src/main/java/com/unitTestimng/unittesting/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.unitTestimng.unittesting.controller;
+
+import com.unitTestimng.unittesting.model.Item;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+}
